f85cdb63c2524907c38205dbeb8f7243946f846b
Kirill V <kirilliuswest@gmail.com>
2021-01-31 12:13:09 -0500

    completed styling

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -17,7 +17,17 @@ class ActivityStore {
     }
 
     @computed get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).sort((a,b) => Date.parse(a.date) - Date.parse(b.date))
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+    }
+
+    groupActivitiesByDate = (activities: IActivity[]) => {
+        const sortedActivities = activities.sort((a,b) => Date.parse(a.date) - Date.parse(b.date));
+        const reducedActivities = sortedActivities.reduce((accumulator, activity: IActivity) => {
+            const date = activity.date.split('T')[0];
+            accumulator[date] = accumulator[date] ? [...accumulator[date], activity] : [activity];
+            return accumulator;
+        }, {} as {[key: string]: IActivity[]});
+        return Object.entries(reducedActivities);
     }
 
     @action loadActivities = async () => {
@@ -29,7 +39,8 @@ class ActivityStore {
                     a.date = a.date.split('.')[0];
                     this.activityRegistry.set(a.id, a);
                 });
-            })
+            });
+            console.log(this.groupActivitiesByDate(activities));
         } catch (error) {
             console.log(error);
         } finally {
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, useState, useContext, useEffect } from 'react';
-import { Button, Form, Segment } from 'semantic-ui-react';
+import { Button, Form, Grid, GridColumn, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 import ActivityStore from '../../../app/stores/activityStore';
@@ -11,9 +11,9 @@ interface DetailParams {
 }
 
 
-const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, history}) => {
+const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({ match, history }) => {
     const activityStore = useContext(ActivityStore);
-    const {createActivity, editActivity, submitting, activity: initialFormState, loadActivity, clearActivity} = activityStore;
+    const { createActivity, editActivity, submitting, activity: initialFormState, loadActivity, clearActivity } = activityStore;
 
     const [activity, setActivity] = useState<IActivity>({
         id: '',
@@ -42,30 +42,35 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, histo
                 ...activity,
                 id: uuid()
             }
-            createActivity(newActivity).then(()=> {history.push(`/activities/${newActivity.id}`)});
+            createActivity(newActivity).then(() => { history.push(`/activities/${newActivity.id}`) });
         } else {
-            editActivity(activity).then(()=> {history.push(`/activities/${activity.id}`)});;
+            editActivity(activity).then(() => { history.push(`/activities/${activity.id}`) });;
         }
     }
 
     const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const {name, value} = event.currentTarget;
-        setActivity({...activity, [name]: value})
+        const { name, value } = event.currentTarget;
+        setActivity({ ...activity, [name]: value })
     }
 
     return (
-        <Segment clearing>
-            <Form onSubmit={handleSubmit}>
-                <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title}/>
-                <Form.TextArea onChange={handleInputChange} name='description' rows={2} placeholder='Description' value={activity.description}/>
-                <Form.Input onChange={handleInputChange} name='category' placeholder='Category' value={activity.category}/>
-                <Form.Input onChange={handleInputChange} name='date' type='datetime-local' placeholder='Date' value={activity.date}/>
-                <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city}/>
-                <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue}/>
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
-                <Button onClick={() => history.push('/activities')} floated='right' type='cancel' content='Cancel'/>
-            </Form>
-        </Segment>
+        <Grid>
+            <GridColumn width={10}>
+                <Segment clearing>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title} />
+                        <Form.TextArea onChange={handleInputChange} name='description' rows={2} placeholder='Description' value={activity.description} />
+                        <Form.Input onChange={handleInputChange} name='category' placeholder='Category' value={activity.category} />
+                        <Form.Input onChange={handleInputChange} name='date' type='datetime-local' placeholder='Date' value={activity.date} />
+                        <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city} />
+                        <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue} />
+                        <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
+                        <Button onClick={() => history.push('/activities')} floated='right' type='cancel' content='Cancel' />
+                    </Form>
+                </Segment>
+            </GridColumn>
+        </Grid>
+
     )
 }
 
